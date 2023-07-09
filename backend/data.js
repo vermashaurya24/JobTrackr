2d7f22faa2775efca2448e2471a05b0e39f8c980
Shaurya Raj Verma <vermashaurya24@gmail.com>
2023-07-09 21:37:39 +0545

    backend updates

---
+++ backend/data.js
@@ -0,0 +1,81 @@
+const applicationsTable = [
+  {
+    id: 1,
+    applications: [
+      {
+        app_id: 1_001,
+        company: "Apple",
+        position: "SDE",
+        application_status: "Round 1",
+      },
+      {
+        app_id: 1_002,
+        company: "Samsung",
+        position: "SDE 2",
+        application_status: "Round 3",
+      },
+      {
+        app_id: 1_003,
+        company: "Google",
+        position: "SDET",
+        application_status: "HR Round",
+      },
+    ],
+  },
+  {
+    id: 2,
+    applications: [
+      {
+        app_id: 2_001,
+        company: "Apple",
+        position: "SDE",
+        application_status: "Round 1",
+      },
+      {
+        app_id: 2_002,
+        company: "Samsung",
+        position: "SDE 2",
+        application_status: "Round 3",
+      },
+      {
+        app_id: 2_003,
+        company: "Google",
+        position: "SDET",
+        application_status: "HR Round",
+      },
+    ],
+  },
+  {
+    id: 3,
+    applications: [
+      {
+        app_id: 3_001,
+        company: "Apple",
+        position: "SDE",
+        application_status: "Round 1",
+      },
+      {
+        app_id: 3_002,
+        company: "Samsung",
+        position: "SDE 2",
+        application_status: "Round 3",
+      },
+      {
+        app_id: 3_003,
+        company: "Google",
+        position: "SDET",
+        application_status: "HR Round",
+      },
+    ],
+  },
+];
+
+const users = [
+  { id: 1, name: "john", password: "1234" },
+  { id: 2, name: "peter", password: "1234" },
+  { id: 3, name: "susan", password: "1234" },
+  { id: 4, name: "anna", password: "1234" },
+  { id: 5, name: "emma", password: "1234" },
+];
+
+module.exports = { applicationsTable, users };
